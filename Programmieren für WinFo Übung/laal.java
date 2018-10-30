6f7032cb019c6d209fc0ebd29530b02bc6d0b9c3
Abcabwehr <pkrebok@googlemail.com>
2018-10-30 12:32:09 +0100

    Neue Übung

---
+++ Programmieren für WinFo Übung/laal.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+ public class laal {
+      public static void main(String[] args) {
+    
+
+  
+   Scanner SC = new Scanner(System.in);
+   System.out.println(" Vorname Bitte ");
+   String Vorname = SC.next();
+   System.out.println("Hallo " + Vorname );
+
+
+
+
+    }
+}
